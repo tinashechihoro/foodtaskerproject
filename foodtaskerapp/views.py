f649153cbd24202d85d0b9b6d4f6143e0e80fbf2
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-04-24 16:56:57 +0200

    home page view

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """homa page"""
+    return render(request,'home.html',{})
