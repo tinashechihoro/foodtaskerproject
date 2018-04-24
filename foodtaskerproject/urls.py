497bdb28cf0523f1625a16f52c492100e0a8a062
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-04-24 18:48:44 +0200

    restaurant sign-in and signout urls

--- foodtaskerproject/urls.py
+++ foodtaskerproject/urls.py
@@ -1,24 +1,16 @@
-"""foodtaskerproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth
 
 from foodtaskerapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^restaurant/sign-in/$', auth.login,
+        {'template_name':'restaurant/sign_in.html'},
+        name='restaurant-sign-in'),
+    url(r'^restaurant/sign-out',auth.logout,
+        {'next_page':'/'},
+        name='restaurant-sign-out')
 ]
