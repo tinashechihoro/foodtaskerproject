ea27db61f3683bd807b5543d0c2e15b40f9de9b6
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-04-25 10:21:39 +0200

    Customer model

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn foodtaskerproject.wsgi --log-file -
--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -16,4 +16,14 @@ class Restaurant(models.Model):
                              blank=False)
     def __str__(self):
         return self.name
+class Customer(models.Model):
+    """Customer model"""
+    user  = models.OneToOneField(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='customer')
+    avatar = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500,blank=True)
+    address = models.CharField(max_length=500,blank=True)
 
+    def __str__(self):
+        return self.user.get_full_name()
